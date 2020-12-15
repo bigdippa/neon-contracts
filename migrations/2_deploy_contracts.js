ec858a8bbf2c72c81f43e379073bf6ed4e1ef8b0
Passion-Power <misoskerlak0124@gmail.com>
2020-12-15 15:59:13 +1000

    Added GovernorAlpha

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ const { ADDRESS } = require('../config');
 const NEONToken = artifacts.require('NEONToken');
 const Presale = artifacts.require('Presale');
 const NEONVault = artifacts.require('NEONVault');
+const GovernorAlpha = artifacts.require('GovernorAlpha');
 // only for test
 const LPTestToken = artifacts.require('LPTestToken');
 
@@ -24,10 +25,13 @@ module.exports = async function (deployer) {
   await deployer.deploy(Presale);
   await deployer.deploy(NEONToken, Presale.address, ADDRESS.AIRDROP_UNISWAP, ADDRESS.AIRDROP_MARKET, ADDRESS.AIRDROP_TEAM);
   await deployer.deploy(NEONVault);
+  await deployer.deploy(GovernorAlpha, ADDRESS.COMP_TIMELOCK, NEONToken.address, ADDRESS.CONTRACT_DEPLOYER);
+
   // only for test
   await deployer.deploy(LPTestToken, ADDRESS.AIRDROP_MARKET);
 
   expertContractJSON('Presale', Presale);
   expertContractJSON('NEONToken', NEONToken);
   expertContractJSON('NEONVault', NEONVault);
+  expertContractJSON('GovernorAlpha', GovernorAlpha);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -25,11 +25,14 @@ module.exports = {
   },
   compilers: {
     solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-      version: '^0.7.0'
+      version: '^0.7.0',
+      docker: false,
+      settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+      }
     }
   }
 };
